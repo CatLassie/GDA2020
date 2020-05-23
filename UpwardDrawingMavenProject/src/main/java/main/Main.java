5214b7a45587d6d9d40203bf3a53c3f1a6dae94e
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2020-05-23 19:59:26 +0200

    - finished parser

--- UpwardDrawingMavenProject/src/main/java/main/Main.java
+++ UpwardDrawingMavenProject/src/main/java/main/Main.java
@@ -3,40 +3,34 @@ package main;
 import static java.lang.System.out;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import util.GraphInstance;
 import util.InputParser;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		String fileName = "test_1";
-		
+
 		String localDir = System.getProperty("user.dir").split("UpwardDrawingMavenProject")[0];
 		// String readPath = localDir + "\\instances\\vc-exact_"+instanceN+".gr";
 		// String writePath = localDir + "\\solutions\\vc-exact_"+instanceN+".gr";
-		String readPath = localDir + "instances\\"+fileName+".json";
+		String readPath = localDir + "instances\\" + fileName + ".json";
 		// out.println(readPath);
 		
-		InputParser inputParser = new InputParser();
-		
+		GraphInstance inst = null;
+
 		try {
-			inputParser.readInstance(readPath);	
-			
-			
-			
-			
-			
-		}
-		catch(FileNotFoundException e) {
+			inst = InputParser.readInstance(readPath);
+
+		} catch (IOException e) {
 			out.println("Sorry, file not found!");
 			System.exit(-1);
 		}
-		
-		
-		
-		
-		out.println("This is the width " + inputParser.getWidth());
+
+		out.println(inst);
 	}
 
 }
