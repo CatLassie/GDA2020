b8d258ec6e24dec203cd872a03e2b7763ec33829
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2020-06-21 04:14:34 +0200

    - cleanup

--- UpwardDrawingMavenProject/src/main/java/model/Solution.java
+++ UpwardDrawingMavenProject/src/main/java/model/Solution.java
@@ -93,71 +93,21 @@ public class Solution {
 	
 	
 	public void positionGraphOnGrid() {
-
 		for (int i = 0; i <= height; i++) {
 			List<Node> nextLayerNodes = layerList.get(i);
 			
-			// currently occupied position on layer
-			// boolean[] occupiedX = new boolean[width+1];
-			
-			/*
-			// straight lines get priority assignment
-			for (int j = 0; j <= width; j++) {
-				if (j < nextLayerNodes.size()) {
-					Node node = nodes.get(nextLayerNodes.get(j)); //TODO: more like find by id but ok for now
-
-					if (!node.assigned && node.dummy && node.pred.dummy) {
-						int diff = node.pred.x - node.pred.pred.x;
-						int x = node.pred.x + diff;
-						// out.println(i+1 + " " + node.pred.x + " " + node.pred.pred.x);
-						if(occupiedX[x] || x > width || x < 0) {
-							out.println("Sorry, couldnt position dummy node " + node.id + " on straight line");
-							System.exit(-1);
-						}
-						node.x = x;
-						occupiedX[x] = true;
-						node.y = i;
-						node.assigned = true;
-					}
-				}
-			}
-			*/
-			
-			/*
-			// dummy nodes get second priority assignment
-			for (int j = 0; j <= width; j++) {
-				if (j < nextLayerNodes.size()) {
-					Node node = nodes.get(nextLayerNodes.get(j)); //TODO: more like find by id but ok for now
-	
-					if (!node.assigned && node.dummy && !node.pred.dummy) {				
-						// find a position for dummy node as close to pred x as possible
-						int x = node.pred.x;
-						int offset = 1;
-						while(occupiedX[x] || x > width || x < 0) {
-							x = offset % 2 == 1 ? x + offset : x - offset;
-							if(offset > width) {
-								out.println("Sorry, couldnt find a position for dummy node " + node.id);
-								System.exit(-1);
-							}
-						}
-						node.x = x;
-						occupiedX[x] = true;
-						node.y = i;
-						node.assigned = true;
-					}
-				}
-			}
-			*/
-			
-			// normal nodes get assigned last
 			for (int j = 0; j <= width; j++) {
 				if (j < nextLayerNodes.size()) {
 					Node node = nodes.get(nextLayerNodes.get(j).id); //TODO: more like find by id but ok for now
 					
+					// OBSOLETE COMMENT!
+                    // DEBUG
+                    // if not equal, the alg. will break!!!
+                    // boolean equal = nextLayerNodes.get(j) == node.id;
+                    // out.println("node idx: " + nextLayerNodes.get(j) + " == node id: " + node.id +" "+ equal );
+					
 					if (!node.assigned) {
-						// first check all dummy nodes and addthem (predecessor x has to be known to calculatenew x)
 						int x = 0;
-						
 						searchX:
 						while(true) {
 							if(x > width) {
@@ -167,6 +117,7 @@ public class Solution {
 							
 							if(!positionOccupied[node.y][x]) {
 								boolean validPosition = true;
+								// check for each edge if it can be positioned with current x
 								for(int k = 0; k < node.pred.size(); k++) {
 									Node pred = node.pred.get(k);
 									validPosition = validPosition && isEdgeFeasible(pred.x, pred.y, x, node.y);
@@ -182,18 +133,14 @@ public class Solution {
 
 							x++;	
 						}
-						//node.x = x;
-						//occupiedX[x] = true;
-						//node.y = i;
-						//node.assigned = true;
 					}
 				}
 			}
 
 		}
-		
 	}
 	
+	// compute all points where edge crosses grid and check if they are occupied
 	private boolean isEdgeFeasible(int x1, int y1, int x2, int y2) {
 		boolean isFeasible = true;
 		double invSlope;
@@ -218,44 +165,6 @@ public class Solution {
 		return isFeasible;
 	}
 	
-		
-	private void addDummyNode(ArrayList<Integer> newSources, int sourceId, int targetId) {
-		/*
-		// create dummy node and add it to nodes and sources
-		// TODO: should find by id!!!
-		Node source = nodes.get(sourceId);
-		Node newDummy = new Node();
-		newDummy.id = nodes.size();
-		newDummy.dummy = true;
-		
-		// all dummy nodes have a pred, and all except last dummy have a succ
-		newDummy.pred = source;
-		if(source.dummy) {
-			source.succ = newDummy;
-		}
-		
-		nodes.add(newDummy);
-		newSources.add(newDummy.id);
-		
-		// update adjacency matrix by rerouting edge through dummy node
-		// add new column
-		for(int i = 0; i < adjacencyMatrix.size(); i++) {
-			adjacencyMatrix.get(i).add(0);
-		}
-		// add new row
-		List<Integer> newRow = new ArrayList<Integer>();
-		for(int i = 0; i < nodes.size(); i++) {
-			newRow.add(0);
-		}
-		adjacencyMatrix.add(newRow);
-		// re-arrange adjacency matrix
-		adjacencyMatrix.get(sourceId).set(targetId, 0);
-		adjacencyMatrix.get(sourceId).set(newDummy.id, 1);
-		adjacencyMatrix.get(newDummy.id).set(targetId, 1);
-		*/
-	}
-	
-
 	private void calculateFeasibility() {
 		out.println("calculateFeasibility()");
 	}
@@ -324,128 +233,4 @@ public class Solution {
 
 		return "Solution:" + feasiblePrint + costPrint + nodesPrint + nodeNumberPrint + edgesPrint  + matrixPrint;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	public void positionGraphOnGridOld() {
-		ArrayList<Integer> nextLayerNodes = new ArrayList<Integer>();
-		for (int i = 0; i <= height; i++) {
-			nextLayerNodes = getNextLayerNodes(nextLayerNodes);
-			
-			// currently occupied position on layer
-			boolean[] occupiedX = new boolean[width+1];
-			
-			// straight lines get priority assignment
-			for (int j = 0; j <= width; j++) {
-				if (j < nextLayerNodes.size()) {
-					Node node = nodes.get(nextLayerNodes.get(j)); //TODO: more like find by id but ok for now
-					
-					// DEBUG
-					// if not equal, the alg. will break!!!
-					// boolean equal = nextLayerNodes.get(j) == node.id;
-					// out.println("node idx: " + nextLayerNodes.get(j) + " == node id: " + node.id +" "+ equal );
-					//
-					
-					if (!node.assigned && node.dummy && node.pred.dummy) {
-						int diff = node.pred.x - node.pred.pred.x;
-						int x = node.pred.x + diff;
-						// out.println(i+1 + " " + node.pred.x + " " + node.pred.pred.x);
-						if(occupiedX[x] || x > width || x < 0) {
-							out.println("Sorry, couldnt position dummy node " + node.id + " on straight line");
-							System.exit(-1);
-						}
-						node.x = x;
-						occupiedX[x] = true;
-						node.y = i;
-						node.assigned = true;
-					}
-				}
-			}
-			
-			// dummy nodes get second priority assignment
-			for (int j = 0; j <= width; j++) {
-				if (j < nextLayerNodes.size()) {
-					Node node = nodes.get(nextLayerNodes.get(j)); //TODO: more like find by id but ok for now
-					
-					// DEBUG
-					// if not equal, the alg. will break!!!
-					// boolean equal = nextLayerNodes.get(j) == node.id;
-					// out.println("node idx: " + nextLayerNodes.get(j) + " == node id: " + node.id +" "+ equal );
-					//
-					
-					if (!node.assigned && node.dummy && !node.pred.dummy) {				
-						// find a position for dummy node as close to pred x as possible
-						int x = node.pred.x;
-						int offset = 1;
-						while(occupiedX[x] || x > width || x < 0) {
-							x = offset % 2 == 1 ? x + offset : x - offset;
-							if(offset > width) {
-								out.println("Sorry, couldnt find a position for dummy node " + node.id);
-								System.exit(-1);
-							}
-						}
-						node.x = x;
-						occupiedX[x] = true;
-						node.y = i;
-						node.assigned = true;
-					}
-				}
-			}
-			
-			// normal nodes get assigned last
-			for (int j = 0; j <= width; j++) {
-				if (j < nextLayerNodes.size()) {
-					Node node = nodes.get(nextLayerNodes.get(j)); //TODO: more like find by id but ok for now
-					
-					// DEBUG
-					// if not equal, the alg. will break!!!
-					// boolean equal = nextLayerNodes.get(j) == node.id;
-					// out.println("node idx: " + nextLayerNodes.get(j) + " == node id: " + node.id +" "+ equal );
-					//
-					
-					if (!node.assigned && !node.dummy) {
-						// first check all dummy nodes and addthem (predecessor x has to be known to calculatenew x)
-						int x = 0;
-						while(occupiedX[x]) {
-							if(x > width) {
-								out.println("Sorry, couldnt find a position for node " + node.id);
-								System.exit(-1);
-							}
-							x++;
-						}
-						node.x = x;
-						occupiedX[x] = true;
-						node.y = i;
-						node.assigned = true;
-					}
-				}
-			}
-
-		}
-	}
-	*/
-	
-	
-	
-
 }
