4edebd7f2af95718accb384384e34b9c711bb66b
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2020-06-22 18:47:46 +0200

    - added cmd line suppoer -  Main cleanup

--- UpwardDrawingMavenProject/src/main/java/main/Main.java
+++ UpwardDrawingMavenProject/src/main/java/main/Main.java
@@ -11,33 +11,18 @@ import util.InputParser;
 import util.SolutionWriter;
 
 public class Main {
-
 	public static void main(String[] args) {
-		/*
-		String instanceN = args[0];
-		String constrType = args[1];
-		String neighbourhoodType = args[2] ;
+		String fileName = args[0];
+		boolean verbose = Boolean.parseBoolean(args[1]);
+		int timeLimit = args.length > 2 ? Integer.parseInt(args[2]) : 180 ;
 		double coolingRate = args.length > 3 ? Double.parseDouble(args[3]) : 0.95;
 		double stoppingCondition = args.length > 4 ? Double.parseDouble(args[4]) : 0.01;
 		int equilibriumCoefficient = 0;
-		*/
-		double coolingRate = 0.95;
-		double stoppingCondition = 0.01;
-		int equilibriumCoefficient = 0;
 		
-		
-
-		String fileName = "auto2";
-		boolean verbose = true;
-		int timeLimit = 180;
-
 		String localDir = System.getProperty("user.dir").split("UpwardDrawingMavenProject")[0];
-		// String readPath = localDir + "\\instances\\vc-exact_"+instanceN+".gr";
-		// String writePath = localDir + "\\solutions\\vc-exact_"+instanceN+".gr";
 		String readPath = localDir + "instances\\" + fileName + ".json";
 		String writePath = localDir + "solutions\\" + fileName + "_solution.json";
 		String writePathOpt = localDir + "solutions\\" + fileName + "_solution_opt.json";
-		// out.println(readPath);
 		
 		GraphInstance inst = null;
 
@@ -45,9 +30,16 @@ public class Main {
 			inst = InputParser.readInstance(readPath);
 			
 			int defaultCoefficient = (int) Math.sqrt(inst.getNodes().size());
-			// equilibriumCoefficient = args.length > 5 ? Integer.parseInt(args[5]) : defaultCoefficient;
-			equilibriumCoefficient = defaultCoefficient;
-
+			equilibriumCoefficient = args.length > 5 ? Integer.parseInt(args[5]) : defaultCoefficient;
+			
+			System.out.println("\nMain.main()");
+			System.out.println("command line args:");
+			System.out.println("file name: "+fileName+"");
+			System.out.println("verbose: "+verbose+"");
+			System.out.println("timeLimit: "+timeLimit+"sec.");
+			System.out.println("cooling rate: "+coolingRate+"");
+			System.out.println("stopping condition: "+stoppingCondition+"");
+			System.out.println("equilibrium coefficient: "+equilibriumCoefficient+"\n");
 		} catch (IOException e) {
 			out.println("Sorry, couldnt open file!");
 			System.exit(-1);
@@ -80,18 +72,12 @@ public class Main {
 			System.exit(-1);
 		}
 		
-		// Write solution to file
+		// Write final solution to file
 		try {
 			SolutionWriter.writeSolution(writePathOpt, bestSolution.getGraphInstanceFromSolution());
 		} catch (IOException e) {
 			out.println("Sorry, couldnt write to file!");
 			System.exit(-1);
 		}
-		
-		
-		// out.println(inst);
-		// out.println("\n"+ solution.getGraphInstanceFromSolution());
-		// out.println("\n" + solution);
 	}
-
 }
