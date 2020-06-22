c252c6a047c9b35689fb202ae288bbb3394c1d29
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2020-06-22 04:53:24 +0200

    - added simulated annealing

--- UpwardDrawingMavenProject/src/main/java/main/Main.java
+++ UpwardDrawingMavenProject/src/main/java/main/Main.java
@@ -2,7 +2,9 @@ package main;
 
 import static java.lang.System.out;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
+import optimization.SimulatedAnnealing;
 import model.GraphInstance;
 import model.Solution;
 import util.InputParser;
@@ -11,6 +13,19 @@ import util.SolutionWriter;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		String instanceN = args[0];
+		String constrType = args[1];
+		String neighbourhoodType = args[2] ;
+		double coolingRate = args.length > 3 ? Double.parseDouble(args[3]) : 0.95;
+		double stoppingCondition = args.length > 4 ? Double.parseDouble(args[4]) : 0.01;
+		int equilibriumCoefficient = 0;
+		*/
+		double coolingRate = 0.95;
+		double stoppingCondition = 0.01;
+		int equilibriumCoefficient = 0;
+		
+		
 
 		String fileName = "auto2";
 		boolean verbose = false;
@@ -26,18 +41,45 @@ public class Main {
 
 		try {
 			inst = InputParser.readInstance(readPath);
+			
+			int defaultCoefficient = (int) Math.sqrt(inst.getNodes().size());
+			// equilibriumCoefficient = args.length > 5 ? Integer.parseInt(args[5]) : defaultCoefficient;
+			equilibriumCoefficient = defaultCoefficient;
+
 		} catch (IOException e) {
 			out.println("Sorry, couldnt open file!");
 			System.exit(-1);
 		}
 		
-		Solution solution = new Solution(inst, verbose);
-		solution.computeInitialFeasibleSolution();
+		Solution initialSolution = new Solution(inst, verbose);
+		initialSolution.computeInitialFeasibleSolution();
+		
+
+		
+		SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(new Solution(initialSolution),
+				   coolingRate, equilibriumCoefficient, stoppingCondition);
+
+		System.out.println("SIMULATED ANNEALING:");
+		System.out.println(simulatedAnnealing+"");
+		double annealingStartCPU = ManagementFactory.getThreadMXBean().getThreadCpuTime(Thread.currentThread().getId());
+		Solution bestSolution = simulatedAnnealing.search();
+		double annealingEndCPU = ManagementFactory.getThreadMXBean().getThreadCpuTime(Thread.currentThread().getId());
+		double annealingDiffCPU = (annealingEndCPU - annealingStartCPU)/1000000000;
+		System.out.println("CPU time: "+annealingDiffCPU+"\n");
+		
+		
+		
+		// Write initial solution to file
+		try {
+			SolutionWriter.writeSolution(writePath, initialSolution.getGraphInstanceFromSolution());
+		} catch (IOException e) {
+			out.println("Sorry, couldnt write to file!");
+			System.exit(-1);
+		}
 		
 		// Write solution to file
 		try {
-			SolutionWriter.writeSolution(writePath, solution.getGraphInstanceFromSolution());
-			// SolutionWriter.writeSolution(writePath, inst);
+			SolutionWriter.writeSolution(writePath, bestSolution.getGraphInstanceFromSolution());
 		} catch (IOException e) {
 			out.println("Sorry, couldnt write to file!");
 			System.exit(-1);
---
+++ UpwardDrawingMavenProject/src/main/java/optimization/SimulatedAnnealing.java
@@ -0,0 +1,97 @@
+package optimization;
+
+import static java.lang.System.out;
+import java.util.concurrent.ThreadLocalRandom;
+
+import model.Node;
+import model.Solution;
+import util.Utilities;
+
+public class SimulatedAnnealing {
+	
+	private Solution bestSolution;
+	private Solution currentSolution;
+	private double temperature; // // f-max - f-min (which is the vertex number)
+	private double coolingRate; // temperature cooling rate (e.g. 0.95)
+	private int equilibriumCondition; // number of moves before temperature adjustment (multiple of vertex number, e.g. n*(sqrt(n)))
+	private double stoppingCondition; // minimum temperature (e.g. 0.01)
+	
+	public SimulatedAnnealing (Solution solution, double coolingRate, int equilibriumCoefficient, double stoppingCondition) {
+		this.bestSolution = new Solution(solution);
+		this.currentSolution = solution;
+		this.temperature = solution.getNodes().size();
+		this.coolingRate = coolingRate;
+		this.equilibriumCondition = equilibriumCoefficient * solution.getNodes().size();
+		this.stoppingCondition = stoppingCondition;
+	}
+	
+	public Solution search() {
+		Utilities.startTimer();
+		while((temperature > stoppingCondition) && !Utilities.isTimeOver()) {
+			oneTemperatureLevelSearch();
+			// System.out.println(temperature);
+			temperature = temperature * coolingRate;	
+		}
+		return currentSolution;
+	}
+	
+	private void oneTemperatureLevelSearch() {
+		equilibriumCycle:
+		for(int i = 0; i < equilibriumCondition; i++) {
+			int randomNodeId = ThreadLocalRandom.current().nextInt(0, currentSolution.getNodes().size());
+			Node randomNode = currentSolution.getNodes().get(randomNodeId);
+			int randomX = ThreadLocalRandom.current().nextInt(0, currentSolution.getWidth() + 1);
+			
+			if(!currentSolution.isMoveFeasible(randomNode, randomX)) {
+				continue equilibriumCycle;
+			}
+			
+			int costChange = currentSolution.computeMoveCost(randomNode, randomX);
+			int newCost = currentSolution.getCost() + costChange;
+			
+			if(newCost < currentSolution.getCost()) {
+				currentSolution.performMove(randomNode, randomX, costChange);
+			} else {
+				double e = java.lang.Math.E;
+				double costDifference = Math.abs(costChange);
+				double metrolopisCriterion = Math.pow(e, -(costDifference / temperature));
+				double probability = (double)ThreadLocalRandom.current().nextInt(0, 1000)/1000;
+				/*
+				if(i == 0) {
+					System.out.println(metrolopisCriterion);
+				}
+				*/
+				if(probability < metrolopisCriterion) {
+					currentSolution.performMove(randomNode, randomX, costChange);
+				}
+			}
+			
+			
+			/*
+			Solution nextSolution = neighbourhood.move(currentSolution);
+			
+			if(nextSolution.getCost() < currentSolution.getCost()) {
+				currentSolution = nextSolution;
+			} else {
+				double e = java.lang.Math.E;
+				double costDifference = Math.abs(nextSolution.getCost() - currentSolution.getCost());
+				double metrolopisCriterion = Math.pow(e, -(costDifference / temperature));
+				double probability = (double)ThreadLocalRandom.current().nextInt(0, 1000)/1000;
+
+				if(probability < metrolopisCriterion) {
+					currentSolution = nextSolution;
+				}
+			}
+			*/
+		}
+		
+	}
+	
+	public String toString() {
+		String annealing = "initial temperature: " + temperature + "\n";
+		annealing += "cooling rate: " + coolingRate + "\n";
+		annealing += "equilibrium condition: " + equilibriumCondition + " iterations\n";
+		annealing += "stopping condition: " + stoppingCondition + " minimum temperature";
+		return annealing;
+	}
+}
